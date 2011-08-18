cf66ecaf09ae264ab494ef073d76756d44a0711f
Tarik Sturm-Dahal <zwillinge14@gmail.com>
2011-08-18 00:32:31 -0400

    Added German lang script

---
+++ src/scripts/de_lang.lua
@@ -0,0 +1,9 @@
+local de = {}
+
+de.BackToMainMenu = 'Zurück zum Hauptmenü'
+de.EnterLobby = 'Lobby eintreten'
+de.GameTitle = 'Verrückte Bahnen'
+de.Ready = 'Bereit'
+de.Quit = 'Abbrechen'
+
+return de
