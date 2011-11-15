ae08bd266091afe0163daa0757345753909e67be
Tarik Sturm-Dahal <zwillinge14@gmail.com>
2011-11-15 02:16:39 -0500

    Maintenance commit - project files rebuilt, build shortcuts updated.

--- GameServer/build/VS2010/x86/cmake_install.cmake
+++ GameServer/build/VS2010/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Typhon/GameServer/src
+# Install script for directory: H:/Code/Typhon/GameServer/src
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -33,7 +33,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "D:/Typhon/GameServer/build/VS2010/x86/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "H:/Code/Typhon/GameServer/build/VS2010/x86/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "D:/Typhon/GameServer/build/VS2010/x86/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "H:/Code/Typhon/GameServer/build/VS2010/x86/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
